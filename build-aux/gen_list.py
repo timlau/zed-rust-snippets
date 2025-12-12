adc805b9c2ca111b0147c9adba36d7681e424f55
Tim Lauridsen <tla@rasmil.dk>
2025-12-12 07:41:24 +0100

    ^ Added list of snippets to readme - add script to generate list of snippets to readme. - clean up descriptions - removed some not needed snippets

---
+++ build-aux/gen_list.py
@@ -0,0 +1,20 @@
+# script to generate a markdown list of snippets to include in README.md
+
+import json
+from pathlib import Path
+from socket import SO_TYPE
+
+input_file = Path("snippets/rust.json")
+
+with input_file.open("r") as in_file:
+    content = json.load(in_file)
+snippet_list = sorted(
+    [(v["prefix"], v.get("description", k)) for k, v in content.items()]
+)
+col1 = "  Snippet"
+col2 = "  Description"
+print("\n## Snippets\n")
+print(f"|{col1:25}|{col2:40}|")
+print(f"|{25 * '-'}|{40 * '-'}|")
+for prefix, description in snippet_list:
+    print(f"|{prefix:25}|{description:40}|")
